c8905d01c4fb69edfa317381cdcdc3c9c2b382a3
KauanAnjos <kauandosanjosmeneses@gmail.com>
2025-07-18 10:46:52 -0300

    corrige nome da propriedade no prompt principal e implementa lógica de seleção de ferramenta

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import prompt from "prompt";
-import mainPrompt from "./prompts/prompt-main";
+import mainPrompt from "./prompts/prompt-main.js";
 
-async function main() {}
+async function main() {
+  prompt.get(mainPrompt, async (err, choose) => {
+    if (choose.select == 1) {
+      console.log("Escolheu o QRCODE");
+      return;
+    } else if (choose.select == 2) {
+      console.log("Escolheu o PASSWORD");
+      return;
+    }
+  });
+
+  prompt.start;
+}
 
 main();
--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -1,6 +1,6 @@
 const mainPrompt = [
   {
-    nome: "select",
+    name: "select",
     description: "Escolha a ferramenta (1- QRCODE ou 2- PASSWORD)",
     pattern: /^[1-2]+$/,
     message: "Escolha apenas entre 1 e 2",
