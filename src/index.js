fab85d43ade7af1a81589907b7850f88467389d5
KauanAnjos <kauandosanjosmeneses@gmail.com>
2025-07-21 15:03:12 -0300

    adiciona a funcionalidade de geração de QR Code e atualiza dependências

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import prompt from "prompt";
 import mainPrompt from "./prompts/prompt-main.js";
-import chalk from "chalk";
+import createQRCode from "./services/qr-code/create.js";
 
 async function main() {
   prompt.get(mainPrompt, async (err, choose) => {
     if (choose.select == 1) {
-      console.log("Escolheu o QRCODE");
+      await createQRCode();
       return;
     } else if (choose.select == 2) {
       console.log("Escolheu o PASSWORD");
