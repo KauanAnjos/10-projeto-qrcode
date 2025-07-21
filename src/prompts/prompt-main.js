9c8d4e3cdeb8dedbcdc474fe0c2e6004f6f34d8c
KauanAnjos <kauandosanjosmeneses@gmail.com>
2025-07-21 16:10:06 -0300

    corrige descrição do prompt principal e implementa lógica de geração de QR Code

--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -4,7 +4,7 @@ const mainPrompt = [
   {
     name: "select",
     description: chalk.yellow.bold(
-      "Escolha a ferramenta (1- QRCODE ou 2- PASSWORD)"
+      "Escolha a ferramenta (1- QRCODE ou (2- PASSWORD"
     ),
     pattern: /^[1-2]+$/,
     message: chalk.red.italic("Escolha apenas entre 1 e 2"),
